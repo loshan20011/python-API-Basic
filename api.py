d349d7c01ec9968b65607fe18675321bc7d80185
loshan <loshan20011@gmail.com>
2024-06-07 09:48:37 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def api():
+    return "Hello"
+
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return jsonify({'Hello': 'world'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
